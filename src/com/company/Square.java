976f7d3559e3246a0aea6f709877e799f647126a
Brian B <brianb@MacBook-Pro-4.local>
2019-01-22 14:46:24 -0500

    Adding Square getters, setters, and calculateArea method

--- src/com/company/Square.java
+++ src/com/company/Square.java
@@ -1,4 +1,25 @@
 package com.company;
 
 public class Square {
+
+    private double height;
+    private double width;
+
+    public void setHeightAndWidth(double enteredLength){
+        height = enteredLength;
+        width = enteredLength;
+    }
+
+    public double getHeight(){
+        return height;
+    }
+
+    public double getWidth(){
+        return width;
+    }
+
+    public double calculateArea(){
+        return height * width;
+    }
+
 }
